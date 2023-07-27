24335ed868382ac37f11f3ad6075cccfb924aa10
Hideaki-Ryu <cenarahmant@gmail.com>
2023-07-28 02:54:28 +0700

    feat: add style for homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// All routes
+const routes = [
+  { path: '/', method: 'GET', description: 'Homepage' },
+  { path: '/api/user', method: 'GET', description: 'Get all users' },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', routes: routes });
 });
 
 module.exports = router;
